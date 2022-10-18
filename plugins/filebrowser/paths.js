c6f903230299474c2b02f45fa7cc30bb4f6d0f98
poulou0 <poulou.0@gmail.com>
2022-10-18 15:37:27 +0200

    filebrowser, set admin password on start-up

--- plugins/filebrowser/paths.js
+++ plugins/filebrowser/paths.js
@@ -3,7 +3,9 @@ module.exports = {
     pathname: "/filebrowser-start",
     commands: ({ROOT_DIR}) => {
       return [
-        "docker-compose -f " + ROOT_DIR + "/plugins/filebrowser/docker-compose.yml up -d",
+        "docker-compose -f " + ROOT_DIR + "/plugins/filebrowser/docker-compose.yml up -d filebrowser_setup",
+        "docker-compose -f " + ROOT_DIR + "/plugins/filebrowser/docker-compose.yml down",
+        "docker-compose -f " + ROOT_DIR + "/plugins/filebrowser/docker-compose.yml up -d filebrowser",
       ];
     }
   },
